eaabcbdab3c8389f42cc839916002e09300bd722
VNDANLAP-0051\LONG <shenlongdemon@gmail.com>
2023-02-28 11:08:44 +0700

    ad core

---
+++ src/screens/portrait/auth/LoginScreen.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from 'react';
+import {View} from '@core/components';
+type Props = {};
+const LoginScreen: FC<Props> = ({}) => {
+  return <View.V >
+
+  </View.V>;
+};
+export default LoginScreen;
